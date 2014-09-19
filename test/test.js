70d7f52d98b55c9e276712886808f0f3aa54695f
Suz Hinton <suz.hinton@gmail.com>
2014-09-18 17:14:39 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -9,3 +9,11 @@ test('should convert image to a buffer', function(t) {
     t.end();
   });
 });
+
+test('buffer should return monochrome pixel data', function(t) {
+  pngtolcd(__dirname + '/cat.png', true, function(err, buffer) {
+    t.ok(!err);
+    t.equal(buffer[0].toString('hex'), 0x00 | 0xFF);
+    t.end();
+  });
+});
