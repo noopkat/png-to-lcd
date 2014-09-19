01a6ec9420d1756e09d36d8c44a0099ca7613cc9
Suz Hinton <suz.hinton@gmail.com>
2014-09-18 17:11:46 -0700

    Updated test to just expect buffer format

--- test/test.js
+++ test/test.js
@@ -1,13 +1,11 @@
 var test = require('tape');
 var pngtolcd = require('../png-to-lcd');
 
-test('should convert image to monochrome buffer', function(t) {
+test('should convert image to a buffer', function(t) {
   pngtolcd(__dirname + '/cat.png', true, function(err, buffer) {
     t.ok(!err);
     //console.log(buffer.toString('hex'));
-
-    //t.equal(buffer[0], 1);
-    //t.equal(buffer[68], 0);
+    t.equal(Buffer.isBuffer(buffer), true);
     t.end();
   });
 });
